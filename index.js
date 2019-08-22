9d3b93c3a0ce7fff38c068cf9a22f60bfb91215b
Daniel Oliveira <oliveiradaniel@protonmail.com>
2019-08-22 15:33:18 -0300

    feat(routes): add proposed middlewares in the challenge

--- index.js
+++ index.js
@@ -5,6 +5,40 @@ const server = express();
 server.use(express.json());
 
 const projects = [];
+let numberOfRequests = 0;
+
+/***************
+ * Middlewares *
+ ***************/
+
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(proj => proj.id === id);
+
+  if (!project) {
+    return res.status(400).json({ error: "Project does not exists" });
+  }
+
+  req.project = project;
+
+  return next();
+}
+
+function logRequests(req, res, next) {
+  console.log(
+    `Number of requests: ${++numberOfRequests}; Method: ${req.method}; URL: ${
+      req.url
+    }`
+  );
+
+  return next();
+}
+
+server.use(logRequests);
+
+/**********
+ * Routes *
+ **********/
 
 server.post("/projects", (req, res) => {
   const { id, title } = req.body;
@@ -24,21 +58,18 @@ server.get("/projects", (req, res) => {
   return res.json(projects);
 });
 
-server.put("/projects/:id", (req, res) => {
-  const { id } = req.params;
+server.put("/projects/:id", checkProjectExists, (req, res) => {
   const { title } = req.body;
-
-  const project = projects.find(proj => proj.id === id);
+  const { project } = req;
 
   project.title = title;
 
   return res.json(projects);
 });
 
-server.delete("/projects/:id", (req, res) => {
-  const { id } = req.params;
+server.delete("/projects/:id", checkProjectExists, (req, res) => {
+  const { project } = req;
 
-  const project = projects.find(project => project.id === id);
   const indexOfProject = projects.indexOf(project);
 
   projects.splice(indexOfProject, 1);
@@ -46,11 +77,9 @@ server.delete("/projects/:id", (req, res) => {
   return res.send();
 });
 
-server.post("/projects/:id/tasks", (req, res) => {
-  const { id } = req.params;
+server.post("/projects/:id/tasks", checkProjectExists, (req, res) => {
   const { title } = req.body;
-
-  const project = projects.find(project => project.id === id);
+  const { project } = req;
 
   project.tasks.push(title);
 
