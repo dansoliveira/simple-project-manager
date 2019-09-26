de4c9379eef9bd7a3def490509ca0a0739b8e365
Daniel Oliveira <oliveiradaniel@protonmail.com>
2019-09-25 23:13:11 -0300

    chore(backend): add database
    Configure Sequelize and add PostgreSQL dependencies

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'DB_DIALECT_HERE',
+  host: 'DB_HOST_HERE',
+  username: 'DB_USERNAME_HERE',
+  password: 'DB_PASSWORD_HERE',
+  database: 'DB_NAME_HERE',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
