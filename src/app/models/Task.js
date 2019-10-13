c015a4255aef2694257a5595ffb509d5d0c85fae
Daniel Oliveira <oliveiradaniel@protonmail.com>
2019-10-13 16:01:59 -0300

    chore: add models and migrations
    Project, Task and User has been add.

---
+++ src/app/models/Task.js
@@ -0,0 +1,16 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Task extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default Task;
