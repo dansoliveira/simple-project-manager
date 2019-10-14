e59e1097ab06a7bb3f7374c9e6c024ce992304fc
Daniel Oliveira <oliveiradaniel@protonmail.com>
2019-10-13 23:14:39 -0300

    feat: add user registration
    Includes JWT authentication.

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'YOUR_SECRET_HERE',
+  expiresIn: 'EXPIRE_DATE_HERE',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 import ProjectController from './app/controllers/ProjectController';
 import TaskController from './app/controllers/TaskController';
 
 const routes = new Router();
+
+routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
 
 routes.get('/projects', ProjectController.index);
 routes.post('/projects', ProjectController.store);
